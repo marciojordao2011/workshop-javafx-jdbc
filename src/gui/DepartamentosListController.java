2f38e0e0f56be20b31678ae8e9c45904c5d27230
unknown <marcio.jordao@hotmail.com>
2022-01-03 15:24:09 -0300

    Update Department

--- src/gui/DepartamentosListController.java
+++ src/gui/DepartamentosListController.java
@@ -3,12 +3,14 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import application.Main;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -16,8 +18,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -28,48 +33,51 @@ import model.entities.Departamentos;
 import model.services.DepartamentosService;
 
 public class DepartamentosListController implements Initializable, DataChangeListener {
-	
+
 	private DepartamentosService service;
-	
+
 	@FXML
-	private TableView<Departamentos> tableViewDepartament;	
-	
+	private TableView<Departamentos> tableViewDepartament;
+
 	@FXML
 	private TableColumn<Departamentos, Integer> tableColumnId;
-	
+
 	@FXML
 	private TableColumn<Departamentos, String> tableColumnNome;
-	
+
+	@FXML
+	private TableColumn<Departamentos, Departamentos> tableColumnEDIT;
+
 	@FXML
 	private Button btNovo;
-	
+
 	private ObservableList<Departamentos> obsList;
-	
+
 	@FXML
 	public void onBtNovoAction(ActionEvent event) {
 		Stage parentStage = Utils.currentStage(event);
 		Departamentos obj = new Departamentos();
 		createDialogForm(obj, "/gui/DepartamentosForm.fxml", parentStage);
 	}
-	
+
 	public void setDepartamentService(DepartamentosService service) {
 		this.service = service;
 	}
-	
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
-		
+
 	}
 
 	private void initializeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnNome.setCellValueFactory(new PropertyValueFactory<>("Nome"));
-		
+
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartament.prefHeightProperty().bind(stage.heightProperty());
 	}
-	
+
 	public void updateTableView() {
 		if (service == null) {
 			throw new IllegalStateException("Serviço estava nulo");
@@ -77,19 +85,20 @@ public class DepartamentosListController implements Initializable, DataChangeLis
 		List<Departamentos> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartament.setItems(obsList);
+		initEditButtons();
 	}
-	
+
 	private void createDialogForm(Departamentos obj, String absoluteName, Stage parentStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
-			
+
 			DepartamentosFormController controller = loader.getController();
 			controller.setDepartamentos(obj);
 			controller.setDepartamentosService(new DepartamentosService());
 			controller.subscribeDataChangeListener(this);
 			controller.updateFormData();
-			
+
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Departament data");
 			dialogStage.setScene(new Scene(pane));
@@ -97,16 +106,42 @@ public class DepartamentosListController implements Initializable, DataChangeLis
 			dialogStage.initOwner(parentStage);
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.showAndWait();
-		}
-		catch(IOException e) {
-		Alerts.showAlert("IOException", "Error loading View", e.getMessage(), AlertType.ERROR);
+		} catch (IOException e) {
+			Alerts.showAlert("IOException", "Error loading View", e.getMessage(), AlertType.ERROR);
 		}
 	}
 
 	@Override
 	public void onDataChaged() {
 		updateTableView();
-		
+
+	}
+
+	public static Optional<ButtonType> showConfirmation(String title, String content) {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(content);
+		return alert.showAndWait();
+	}
+
+	private void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Departamentos, Departamentos>() {
+			private final Button button = new Button("edit");
+
+			@Override
+			protected void updateItem(Departamentos obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(
+						event -> createDialogForm(obj, "/gui/DepartamentosForm.fxml", Utils.currentStage(event)));
+			}
+		});
 	}
 
 }
